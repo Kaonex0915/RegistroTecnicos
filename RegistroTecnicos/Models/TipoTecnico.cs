040304cc3a27a3c16847578412e7e99c1f03c8b0
Marcos Duran <Marcos_Duran@ucne.edu.do>
2024-09-05 22:42:45 -0400

    Crendo el modelo del Tipo de tecnicos

---
+++ RegistroTecnicos/Models/TipoTecnico.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RegistroTecnicos.Models;
+
+public class TipoTecnico
+{
+    [Key]
+    public int TipoTecnicoId { get; set; }
+    [ForeignKey("TecnicoId")]
+    public int TecnicoId { get; set; }
+
+    [Required(ErrorMessage = "El tipo de tecnico es nesesario")]
+    public string Descripcion { get; set; }
+    
+    
+}
+
