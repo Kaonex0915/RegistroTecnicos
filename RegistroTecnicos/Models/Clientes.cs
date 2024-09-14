4cb4ccd11d8c514dfb29f7e5851060420573bfff
Marcos Duran <Marcos_Duran@ucne.edu.do>
2024-09-14 00:04:21 -0400

    Creando los modelos de Clientes y Trabajos

---
+++ RegistroTecnicos/Models/Clientes.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace RegistroTecnicos.Models
+{
+    public class Clientes
+    {
+        [Key]
+        public int ClientesId { get; set; }     
+        [Required(ErrorMessage = "El nombre del cliente es necesario")]
+        public string Nombres { get; set; }
+
+        [Required(ErrorMessage = "El campo Whatsapp es necesario")]
+        [Range(10, 12)]
+        public string Whatsapp { get; set; }
+
+    }
+}
---
+++ RegistroTecnicos/Models/Trabajos.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RegistroTecnicos.Models
+{
+    public class Trabajos
+    {
+        [Key]
+        public int TrabajosId { get; set; }
+        [ForeignKey("ClientesId")]
+        public int ClientesId { get; set; }
+
+        [ForeignKey("TecnicoId")]
+        public int TecnicoId { get; set;}
+
+        [Required(ErrorMessage = "La fecha del trabajo es requerida")]
+        public DateTime Fecha {  get; set; }
+
+        [Required(ErrorMessage ="La descriocion del trabajo es requerida")]
+        public string? Descripcion { get; set; }
+
+        [Required(ErrorMessage ="El monto del trabajo es requerido")]
+        public int Monto { get; set; }
+        
+
+    }
+}
