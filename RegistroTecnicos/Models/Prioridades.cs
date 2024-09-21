ea7bd1a8bf27ec7a0dd135ed8977fd8f294f27ad
Marcos Duran <Marcos_Duran@ucne.edu.do>
2024-09-20 22:17:02 -0400

    Creando modelo de prioridades

---
+++ RegistroTecnicos/Models/Prioridades.cs
@@ -0,0 +1,16 @@
+﻿
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace RegistroTecnicos.Models
+{
+    public class Prioridades
+    {
+        [Key]
+        public int PrioridadId { get; set; }
+        [Required(ErrorMessage = "La descripcion es requerida")]
+        public string Descripcion { get; set; }
+        [Required(ErrorMessage ="La fecha es requerida")]
+        public int Fecha {  get; set; }  
+    }
+}
